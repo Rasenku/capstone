f9d157a2139790298e72bc918049bfe6d2368ce4
Dahveyea <dahveyea.cowan@gmail.com>
2020-12-03 21:58:22 -0800

    Added Docker compose

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
 
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,3 +16,6 @@ def anime():
 def AboutAnime():
     """Return homepage."""
     return render_template('AboutAnime.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
